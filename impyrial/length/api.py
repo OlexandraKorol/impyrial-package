ae48799be9016005a197e101bdca22355522a24b
Oleksandra <korol.olexandra@gmail.com>
2026-04-13 15:59:29 +0300

    setting-exports

--- impyrial/length/api.py
+++ impyrial/length/api.py
@@ -1,22 +1,19 @@
-("in", "ft", "yd")
-
-from .core import inches_to_feet, inches_to_yards, UNITS
+from . import core
 from ..utils import convert_to_meters
 
-
 def convert_unit(x, from_unit, to_unit):
-    if from_unit not in UNITS or to_unit not in UNITS:
+    if from_unit not in core.UNITS or to_unit not in core.UNITS:
         raise ValueError("Unsupported unit, please choose from: in, ft, yd")
 
     if from_unit == "ft":
-        x = inches_to_feet(x, reverse=True)
+        x = core.inches_to_feet(x, reverse=True)
     elif from_unit == "yd":
-        x = inches_to_yards(x, reverse=True)
+        x = core.inches_to_yards(x, reverse=True)
 
     if to_unit == "ft":
-        return inches_to_feet(x)
+        return core.inches_to_feet(x)
     elif to_unit == "yd":
-        return inches_to_yards(x)
+        return core.inches_to_yards(x)
     elif to_unit == "in":
         return x
     elif to_unit == "m":
--- impyrial/length/core.py
+++ impyrial/length/core.py
@@ -1,6 +1,9 @@
+from ..utils import helper_function
+
 UNITS = ("in", "ft", "yd")
 
 def inches_to_feet(x, reverse=False):
+    print(f"Test imports: {helper_function(x)}")
     if reverse:
         return x * 12
     return x / 12
@@ -11,3 +14,4 @@ def inches_to_yards(x, reverse=False):
         return x * 36
     return x / 36
 
+inches_to_feet(3)
